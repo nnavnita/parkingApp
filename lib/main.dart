33ffa8cb8567a1668ad119cbd8d6ac9de0714e79
nnavnita <navnitanandakumar@outlook.com>
2021-01-19 00:11:38 +1100

    map with spots in green/red to show availability

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,64 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
-import 'apicall.dart';
+Future<Bay> fetchBay(int i) async {
+  final response = await http
+      .get('https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json');
+
+  if (response.statusCode == 200) {
+    return Bay.fromJson(jsonDecode(response.body), i);
+  } else {
+    throw Exception('Failed to load bay');
+  }
+}
+
+class Bay {
+  final String bayID;
+  final String stMarkerID;
+  final String status;
+  final String lat;
+  final String lon;
+  factory Bay.fromJson(List<dynamic> json, int i) {
+    return Bay(
+      bayID: json[i]['bayID'],
+      stMarkerID: json[i]['stMarkerID'],
+      status: json[i]['status'],
+      lat: json[i]['lat'],
+      lon: json[i]['lon'],
+    );
+  }
+
+  Bay({this.bayID, this.stMarkerID, this.status, this.lat, this.lon});
+}
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Future<Bay> futureBay;
+  int i;
+  LatLng _bayLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    i = 0;
+    futureBay = fetchBay(i);
+  }
+
   Completer<GoogleMapController> _controller = Completer();
-
-  static const LatLng _center = const LatLng(-37.814231066918644, 144.9394480531859);
-
+  
   final Set<Marker> _markers = {};
-
-  LatLng _lastMapPosition = _center;
 
   MapType _currentMapType = MapType.normal;
 
@@ -31,23 +70,30 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  void _onAddMarkerButtonPressed() {
+  void _onAddMarkerButtonPressed(String status, LatLng location) {
+    var marker = BitmapDescriptor.defaultMarker;
+    String titleString = "Unavailable";
+    if (status == "Unoccupied") {
+      marker = BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen);
+      titleString = "Available";
+    }
     setState(() {
+      i= i+1;
+      futureBay = fetchBay(i); 
+      _bayLocation = location;
       _markers.add(Marker(
-        // This marker id can be anything that uniquely identifies each marker.
-        markerId: MarkerId(_lastMapPosition.toString()),
-        position: _lastMapPosition,
+        markerId: MarkerId(_bayLocation.toString()),
+        position: _bayLocation,
         infoWindow: InfoWindow(
-          title: 'Really cool place',
-          snippet: '5 Star Rating',
+          title: titleString,
         ),
-        icon: BitmapDescriptor.defaultMarker,
+        icon: marker,
       ));
     });
   }
 
   void _onCameraMove(CameraPosition position) {
-    _lastMapPosition = position.target;
+    _bayLocation = position.target;
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -62,44 +108,56 @@ class _MyAppState extends State<MyApp> {
           title: Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
-              mapType: _currentMapType,
-              markers: _markers,
-              onCameraMove: _onCameraMove,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: Column(
-                  children: <Widget> [
-                    FloatingActionButton(
-                      onPressed: _onMapTypeButtonPressed,
-                      materialTapTargetSize: MaterialTapTargetSize.padded,
-                      backgroundColor: Colors.green,
-                      child: const Icon(Icons.map, size: 36.0),
+        body: Center(
+          child: FutureBuilder<Bay>(
+            future: futureBay,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                _bayLocation = LatLng(double.parse(snapshot.data.lat), double.parse(snapshot.data.lon));
+                return Stack(
+                  children: <Widget>[
+                    GoogleMap(
+                      onMapCreated: _onMapCreated,
+                      initialCameraPosition: CameraPosition(
+                        target: _bayLocation,
+                        zoom: 11.0,
+                      ),
+                      mapType: _currentMapType,
+                      markers: _markers,
+                      onCameraMove: _onCameraMove,
                     ),
-                    SizedBox(height: 16.0),
-                    FloatingActionButton(
-                      onPressed: _onAddMarkerButtonPressed,
-                      materialTapTargetSize: MaterialTapTargetSize.padded,
-                      backgroundColor: Colors.green,
-                      child: const Icon(Icons.add_location, size: 36.0),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Align(
+                        alignment: Alignment.topRight,
+                        child: Column(
+                          children: <Widget> [
+                            FloatingActionButton(
+                              onPressed: _onMapTypeButtonPressed,
+                              materialTapTargetSize: MaterialTapTargetSize.padded,
+                              backgroundColor: Colors.green,
+                              child: const Icon(Icons.map, size: 36.0),
+                            ),
+                            SizedBox(height: 16.0),
+                            FloatingActionButton(
+                              onPressed: () => _onAddMarkerButtonPressed(snapshot.data.status, _bayLocation),
+                              materialTapTargetSize: MaterialTapTargetSize.padded,
+                              backgroundColor: Colors.green,
+                              child: const Icon(Icons.add_location, size: 36.0),
+                            )
+                          ]
+                        )
+                      )
+                    )
+                  ]
+                );
+              } else {
+                return Container();
+              }
+            }
+          )
+        )
+      )
     );
   }
 }
